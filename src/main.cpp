0b3f0d35280d1b4628236910134a49e7de8d8dc7
V Hansen Halim <fpsecond.hh@gmail.com>
2023-07-09 00:22:45 +0700

    add second motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <Arduino.h>
 #include <AFMotor.h>
 
-AF_Stepper motor(48, 2);
+AF_Stepper Stepper1(48, 1);
+AF_Stepper Stepper2(48, 2);
 
 void setup()
 {
-  motor.setSpeed(100); // 100 rpm
+  Stepper1.setSpeed(50); // 50 rpm
+  Stepper2.setSpeed(50); // 50 rpm
 }
 
 void loop()
 {
-  motor.step(500, FORWARD, SINGLE);
-  motor.step(500, BACKWARD, SINGLE);
-  delay(5000);
+  Stepper1.step(500, FORWARD, DOUBLE);
+  Stepper1.step(500, BACKWARD, DOUBLE);
+  Stepper2.step(500, FORWARD, DOUBLE);
+  Stepper2.step(500, BACKWARD, DOUBLE);
+  // delay(5000);
 }
